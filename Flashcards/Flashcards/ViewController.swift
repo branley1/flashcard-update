3bdb658d2ee79d8ac8315e4d031fee60d5664a51
Branley Mmasi <mmasi.branley@gmail.com>
2023-04-05 06:47:54 -0400

    flashcard-update1

---
+++ Flashcards/Flashcards/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Branley Mmasi on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+}
+
